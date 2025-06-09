baf70f228e1d0ec8e081a0839060f61f4f54fb11
croco dendy <croco@deimos.adoo.one.tld>
2025-06-09 21:02:11 +0000

    fix(project): structure

--- apps/wave/ecosystem.config.js
+++ apps/wave/ecosystem.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   apps: [
     {
-      name: 'radio.core',
+      name: 'radio.wave',
       script: 'src/index.ts',
       interpreter: 'bun',
       env_production: { PORT: 6970, SOCKET_PORT: 6971 },
